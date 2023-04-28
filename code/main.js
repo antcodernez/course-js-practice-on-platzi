4d30dbc496fad46105b25cb8efb0525ce3012096
Antonio Jiménez <marshday5@gmail.com>
2023-04-28 16:53:35 -0600

    I make the mobile menu

--- code/main.js
+++ code/main.js
@@ -1,11 +1,24 @@
+// menu desktop
 const menuEmail = document.querySelector(".navbar-right__email");
 const menuFlechita = document.querySelector(".flechita");
 const menuDesktop = document.querySelector(".desktop-menu");
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuFlechita.addEventListener("click", toggleDesktopMenu)
 
-
- function toggleDesktopMenu()
+function toggleDesktopMenu()
     {
         menuDesktop.classList.toggle("inactive");
     }
+// menu mobile
+const menuHamgurber = document.querySelector(".icon-menu");
+const menuMobile = document.querySelector(".mobile-menu");
+
+menuHamgurber.addEventListener("click", () => {menuMobile.classList.toggle("mobile-menu-translate");});
+
+window.addEventListener("click", e => 
+    {
+        if(menuMobile.classList.contains("mobile-menu-translate") && e.target != menuMobile && e.target != menuHamgurber)
+        {
+            menuMobile.classList.toggle("mobile-menu-translate");
+        }
+    });
