814a7812d9113c1b944c7e7faef411a112a0e49f
Antonio Jiménez <marshday5@gmail.com>
2023-04-28 23:29:44 -0600

    I'm making the shopping cart with some caracteristics on mobile and desktop to have a better experience on the app

--- code/main.js
+++ code/main.js
@@ -15,10 +15,25 @@ const menuMobile = document.querySelector(".mobile-menu");
 
 menuHamgurber.addEventListener("click", () => {menuMobile.classList.toggle("mobile-menu-translate");});
 
-window.addEventListener("click", e => 
+window.addEventListener("click", e =>
     {
         if(menuMobile.classList.contains("mobile-menu-translate") && e.target != menuMobile && e.target != menuHamgurber)
         {
             menuMobile.classList.toggle("mobile-menu-translate");
         }
     });
+
+// shooping cart
+const menuCartIcon = document.querySelector(".navbar-right__shooping-cart");
+const shoopingCart = document.querySelector(".main-content");
+const returnBtnShoppingCart = document.querySelector(".return-button-main-content");
+
+menuCartIcon.addEventListener("click", () => { shoopingCart.classList.toggle("main-content--active")});
+returnBtnShoppingCart.addEventListener("click", () => { shoopingCart.classList.toggle("main-content--active")});
+
+// window.addEventListener("click", e => {
+//     if(shoopingCart.classList.contains("main-content--active") && e.target != shoopingCart && e.target != menuCartIcon)
+//         {
+//             shoopingCart.classList.toggle("main-content--active");
+//         }
+// });
